e5e2cc085c7377736f21ebb67552e99d74050c3e
shaifur <shaifur.at.buet@gmail.com>
2018-03-08 14:28:10 -0500

    init commit

--- temp/citygml_parser.py
+++ temp/citygml_parser.py
@@ -1,8 +1,17 @@
+'''
+author: Md Shaifur Rahman (mdsrahman@cs.stonybrook.edu)
+        Stony Brook University, NY.
+'''
 
 
 class CityGMLParser(object):
     '''
-
+    Given a citygml file (.gml) this class
+        1. parses the 3D building structure and save it
+        2. visualize the 3D structures (not recommended for large swathes of area)
+        3. calculate the line-of-sight assuming one FSO antenna at the roof-top of everybuilding
+        4. calculate coverage assuming one base-station at the ground level of every building
+        4. save the above graphs for future flow-level computation of FSONet
     '''
     def __init__(self):
         return
